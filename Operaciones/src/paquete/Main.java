6fc1145a0d8267360785fbe5f6782e91cf58f075
AllberMC <32437364+AllberMC@users.noreply.github.com>
2017-10-23 11:32:34 +0100

    Operaciones Aritmeticas

---
+++ Operaciones/src/paquete/Main.java
@@ -0,0 +1,28 @@
+package paquete;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var1=10;
+		int var2=3;
+		
+		int suma;
+		suma = var1+var2;
+		int resta;
+		resta = var1-var2;
+		float mult;
+		mult=var1*var2;
+		double div;
+		div=var1/var2;
+		double residuo;
+		residuo=var1%var2;
+		
+		System.out.println("El resultado es: " + suma); //Para añadir una var a un cadena de texto se utiliza el +
+		System.out.println("El resultado es: " + resta);
+		System.out.println("El resultado es: " + mult);
+		System.out.println("El resultado es: " + div);
+		System.out.println("El resultado es: " + residuo);
+	}
+
+}
