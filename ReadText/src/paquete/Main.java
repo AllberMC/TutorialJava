68d2cbb150372f15774bce6525a90af138a4791c
AllberMC <32437364+AllberMC@users.noreply.github.com>
2017-10-25 18:35:41 +0100

    Leer archivos txt y mostrar su contenido

---
+++ ReadText/src/paquete/Main.java
@@ -0,0 +1,14 @@
+package paquete;
+import java.io.*;
+import paquete.Texto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Texto t = new Texto();
+		t.leer("archivo.txt");
+		
+	}
+
+}
